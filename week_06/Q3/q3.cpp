bc5b6065c4d7d278be94660f022ca280109cf81d
rohitparihar-code <rohitparihar2002@gmail.com>
2021-11-02 18:17:06 +0530

    Pushed Q3

---
+++ week_06/Q3/q3.cpp
@@ -0,0 +1,88 @@
+// Program to implement Type Checking
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct symbol_table
+{
+    int type;
+    char var[10];
+} st[50];
+
+int c = 0;
+
+void sep(string s)
+{
+    int l = s.length();
+    int i, j = 0;
+    char temp[50];
+    string tp;
+    for (i = 0; i < l && s[i] != ' '; ++i)
+    {
+        tp.push_back(s[i]);
+    }
+    temp[0] = '\0';
+    ++i;
+    for (; i < l; ++i)
+    {
+        if (s[i] != ',' && s[i] != ' ' && s[i] != ';')
+            temp[j++] = s[i];
+        else
+        {
+            if (tp == "int")
+                st[c].type = 1;
+            else if (tp == "float")
+                st[c].type = 2;
+            strcpy(st[c++].var, temp);
+            temp[0] = '\0';
+            j = 0;
+        }
+    }
+}
+int check(char a[])
+{
+    int len = strlen(a);
+    int i, j = 0, key = 0, k;
+    char temp[50];
+    for (i = 0; i < len; ++i)
+    {
+        if (a[i] != 32 && a[i] != '+' && a[i] != '=' && a[i] != ';')
+            temp[j++] = a[i];
+        else
+        {
+            temp[j] = '\0';
+            for (k = 0; k < c; ++k)
+            {
+                if (strcmp(st[k].var, temp) == 0)
+                {
+                    if (key == 0)
+                        key = st[k].type;
+                    else if (st[k].type != key)
+                        return 0;
+                }
+            }
+            j = 0;
+        }
+    }
+    return 1;
+}
+int main()
+{
+    int N, ans, i;
+    char s[50];
+    printf("\nEnter the total lines of declaration\n");
+    scanf("%d", &N);
+    while (N--)
+    {
+        scanf(" %[^\n]", s);
+        sep(s);
+    }
+    printf("Enter the expression:\n");
+    scanf(" %[^\n]", s);
+    if (check(s))
+        printf("Correct\n");
+    else
+        printf("Semantic error\n");
+
+    return 0;
+}
---
+++ week_06/Q3/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    float c;
+    b = 10;
+    c = 5.5;
+    a = b + c;
+    printf("%d\n", a);
+    return 0;
+}
