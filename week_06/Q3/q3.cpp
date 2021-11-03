1320a295fbedc3c3269f14f27012e085084b99fb
rohitparihar-code <rohitparihar2002@gmail.com>
2021-11-03 12:50:18 +0530

    Q3  formats

--- week_06/Q3/q3.cpp
+++ week_06/Q3/q3.cpp
@@ -8,10 +8,13 @@ struct symbol_table
     int type;
     char var[10];
 } st[50];
+// Type 1: int
+// Type 2: float
+// Type 3: char
 
 int c = 0;
 
-void sep(string s)
+void extract(string s)
 {
     int l = s.length();
     int i, j = 0;
@@ -33,20 +36,26 @@ void sep(string s)
                 st[c].type = 1;
             else if (tp == "float")
                 st[c].type = 2;
+            else if (tp == "char")
+                st[c].type = 3;
             strcpy(st[c++].var, temp);
             temp[0] = '\0';
             j = 0;
         }
     }
 }
-int check(char a[])
+
+map<string, map<string, int>> c_pairs;
+
+bool check(char a[])
 {
     int len = strlen(a);
     int i, j = 0, key = 0, k;
     char temp[50];
+    string prev = "";
     for (i = 0; i < len; ++i)
     {
-        if (a[i] != 32 && a[i] != '+' && a[i] != '=' && a[i] != ';')
+        if (a[i] != ' ' && a[i] != '+' && a[i] != '=' && a[i] != ';' && a[i] != '*' && a[i] != '%' && a[i] != '/')
             temp[j++] = a[i];
         else
         {
@@ -58,17 +67,18 @@ int check(char a[])
                     if (key == 0)
                         key = st[k].type;
                     else if (st[k].type != key)
-                        return 0;
+                        return false;
                 }
             }
             j = 0;
         }
     }
-    return 1;
+    return true;
 }
+
 int main()
 {
-    int n, ans, i;
+    int n, i;
     char s[50];
     printf("\nEnter the total lines of declaration (Ex: int a, b, c; is one line of declaration): ");
     scanf("%d", &n);
@@ -78,8 +88,22 @@ int main()
     {
         cout << i + 1 << ": ";
         scanf(" %[^\n]", s);
-        sep(s);
+        extract(s);
         i++;
+    }
+
+    {
+        c_pairs["int"]["float"] = 1;
+        c_pairs["float"]["int"] = 1;
+        c_pairs["double"]["float"] = 1;
+        c_pairs["double"]["int"] = 1;
+        c_pairs["float"]["double"] = 1;
+        c_pairs["int"]["double"] = 1;
+        c_pairs["string"]["string"] = 1;
+        c_pairs[""]["int"] = 1;
+        c_pairs[""]["float"] = 1;
+        c_pairs[""]["double"] = 1;
+        c_pairs[""]["string"] = 1;
     }
 
     int choice;
@@ -98,14 +122,13 @@ int main()
 
         cout << "Result: ";
         if (check(s))
-            {
-                cout << "No Errors Found\n\n";
-            }
+        {
+            cout << "No Errors Found\n\n";
+        }
         else
-            {
-                cout << "Semantic Error Will Occur\n\n";
-            }
-        
+        {
+            cout << "Semantic Error Will Occur\n\n";
+        }
     }
 
     return 0;
