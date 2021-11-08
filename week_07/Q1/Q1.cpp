734a4f13d66fdd1251d98a63af2f6196e15a8977
Prajwaljm <prajwaljm.7s@gmail.com>
2021-11-08 16:55:35 +0530

    added indirect triples

--- week_07/Q1/Q1.cpp
+++ week_07/Q1/Q1.cpp
@@ -164,6 +164,88 @@ void triple()
 
 void indirect_triple()
 {
+    char g;
+    scanf("%c", &g);
+    char line[20];
+    int s[20];
+    int t = 100;
+    int i = 0;
+
+    printf("\nEnter Expression: ");
+    gets(line);
+    
+    for (i = 0; i < 20; i++)
+        s[i] = 0;
+    
+    // Op -> Operator
+    // a1 -> Argument 1
+    // a2 -> Argument 2
+    // re -> Result
+    printf("no\top\ta1\ta2\n");
+    
+    for (i = 2; line[i] != '\0'; i++)
+    {
+        if (line[i] == '/' || line[i] == '*')
+        {
+            printf("\n");
+            if (s[i] == 0)
+            {
+                if (s[i + 1] == 0)
+                {
+                    printf("%d \t= \t%c\n", t, line[i + 1], t);
+                    s[i + 1] = t++;
+                }
+    
+                printf("%d \t%c\t", t, line[i]);
+    
+                (s[i - 1] == 0) 
+                    ? printf("%c\t", line[i - 1]) 
+                    : printf("%d\t", s[i - 1]);
+    
+                printf("%d", s[i + 1]);
+                
+                s[i - 1] = s[i + 1] = t++;
+                s[i] = 1;
+            }
+        }
+    }
+
+    for (i = 2; line[i] != '\0'; i++)
+    {
+        if (line[i] == '+' || line[i] == '-')
+        {
+            printf("\n");
+            
+            if (s[i] == 0)
+            {
+                if (s[i + 1] == 0)
+                {
+                    printf("%d \t= \t%c\t%d\n", t, line[i + 1], t);
+                    s[i + 1] = t++;
+                }
+            
+                printf("%d \t%c\t", t, line[i]);
+            
+                (s[i - 1] == 0) 
+                    ? printf("%c\t", line[i - 1]) 
+                    : printf("%d\t", s[i - 1]);
+            
+                printf("%d", s[i + 1]);
+            
+                s[i - 1] = s[i + 1] = t++;
+                s[i] = 1;
+            }
+        }
+    }
+
+    printf("\n%d \t= \t%d\t%c\n", t, t - 1, line[0]);
+
+    printf("\nno\tStatement no\n");
+    for(i=0; i<=t-100; i++)
+    {
+        printf("%d \t%d\n", i, i+100);
+    }
+    cout<<"\n";
 }
 
 int main()
@@ -186,33 +268,12 @@ int main()
             break;
         case 3:
             indirect_triple();
+            break;
         default:
             cout << "\nInvalid Choice... Please Try Again\n";
             break;
         }
     }
-    // do
-    // {
-    //     cout << "\n1. Quadruple\n2. Triple\n3. Indirect Triple\n0. Exit\nEnter your choice: ";
-    //     cin >> choice;
-    //     switch (choice)
-    //     {
-    //     case 0:
-    //         cout << "\nProgram Quit...\nBye (:\n\n";
-    //         return 0;
-    //     case 1:
-    //         copy_quadruple();
-    //         break;
-    //     case 2:
-    //         triple();
-    //         break;
-    //     case 3:
-    //         indirect_triple();
-    //     default:
-    //         cout << "\nInvalid Choice... Please Try Again\n";
-    //         break;
-    //     }
-    // } while (choice != 0);
 
     return 0;
 }
