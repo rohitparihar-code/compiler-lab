a089d03d2449c2a9c565ebb50e39b82cf1f6369a
rohitparihar-code <rohitparihar2002@gmail.com>
2021-11-10 09:47:17 +0530

    changed struct variable names

--- week_07/Q3/Q3.cpp
+++ week_07/Q3/Q3.cpp
@@ -6,8 +6,8 @@
 
 struct expression
 {
-    char l;
-    char r[20];
+    char lhs;
+    char rhs[20];
 } exp[10], res[10];
 
 int main()
@@ -24,62 +24,61 @@ int main()
     for (i = 0; i < n; i++)
     {
         printf("Expression %d : ", i + 1);
-        exp[i].l = getche();
-        scanf("%c%s", &gb, exp[i].r);
+        exp[i].lhs = getche();
+        scanf("%c%s", &gb, exp[i].rhs);
     }
 
     // printf("\nIntermediate Code :\n");
     printf("\n\n");
     for (i = 0; i < n; i++)
     {
-        printf("%c", exp[i].l);
-        printf("%s\n", exp[i].r);
+        printf("%c", exp[i].lhs);
+        printf("%s\n", exp[i].rhs);
     }
 
     for (i = 0; i < n - 1; i++)
     {
-        temp = exp[i].l;
+        temp = exp[i].lhs;
         for (j = 0; j < n; j++)
         {
-            p = strchr(exp[j].r, temp);
+            p = strchr(exp[j].rhs, temp);
             if (p)
             {
-                res[z].l = exp[i].l;
-                strcpy(res[z].r, exp[i].r);
+                res[z].lhs = exp[i].lhs;
+                strcpy(res[z].rhs, exp[i].rhs);
                 z++;
             }
         }
     }
 
-    res[z].l = exp[n - 1].l;
-    strcpy(res[z].r, exp[n - 1].r);
+    res[z].lhs = exp[n - 1].lhs;
+    strcpy(res[z].rhs, exp[n - 1].rhs);
     z++;
 
     printf("\nAfter dead code Removal :\n");
     for (k = 0; k < z; k++)
     {
-        printf("%c", res[k].l);
-        printf("%s\n", res[k].r);
+        printf("%c", res[k].lhs);
+        printf("%s\n", res[k].rhs);
     }
 
-    //sub expression elimination
     for (m = 0; m < z; m++)
     {
-        tem = res[m].r;
+        tem = res[m].rhs;
         for (j = m + 1; j < z; j++)
         {
-            p = strstr(tem, res[j].r);
+            p = strstr(tem, res[j].rhs);
             if (p)
             {
-                t = res[j].l;
-                res[j].l = res[m].l;
+                t = res[j].lhs;
+                res[j].lhs = res[m].lhs;
                 for (i = 0; i < z; i++)
                 {
-                    l = strchr(res[i].r, t);
+                    l = strchr(res[i].rhs, t);
                     if (l)
                     {
-                        a = l - res[i].r;
-                        res[i].r[a] = res[m].l;
+                        a = l - res[i].rhs;
+                        res[i].rhs[a] = res[m].lhs;
                     }
                 }
             }
@@ -89,8 +88,8 @@ int main()
     printf("\nAfter eliminating common expression :\n");
     for (i = 0; i < z; i++)
     {
-        printf("%c", res[i].l);
-        printf("%s\n", res[i].r);
+        printf("%c", res[i].lhs);
+        printf("%s\n", res[i].rhs);
     }
 
     // duplicate production elimination
@@ -98,11 +97,11 @@ int main()
     {
         for (j = i + 1; j < z; j++)
         {
-            q = strcmp(res[i].r, res[j].r);
-            if ((res[i].l == res[j].l) && !q)
+            q = strcmp(res[i].rhs, res[j].rhs);
+            if ((res[i].lhs == res[j].lhs) && !q)
             {
-                res[i].l = '\0';
-                strcpy(res[i].r, "\0");
+                res[i].lhs = '\0';
+                strcpy(res[i].rhs, "\0");
             }
         }
     }
@@ -110,10 +109,10 @@ int main()
     printf("\nOptimized code :\n");
     for (i = 0; i < z; i++)
     {
-        if (res[i].l != '\0')
+        if (res[i].lhs != '\0')
         {
-            printf("%c", res[i].l);
-            printf("%s\n", res[i].r);
+            printf("%c", res[i].lhs);
+            printf("%s\n", res[i].rhs);
         }
     }
 
