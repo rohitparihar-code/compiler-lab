18b18b27f0ae7dfad1b991e2e389708246664d13
rohitparihar-code <rohitparihar2002@gmail.com>
2021-11-10 09:55:17 +0530

    resolved minor warning

--- week_07/Q3/Q3.cpp
+++ week_07/Q3/Q3.cpp
@@ -8,7 +8,7 @@ struct expression
 {
     char lhs;
     char rhs[20];
-} exp[10], res[10];
+} expr[10], res[10];
 
 int main()
 {
@@ -24,35 +24,35 @@ int main()
     for (i = 0; i < n; i++)
     {
         printf("Expression %d : ", i + 1);
-        exp[i].lhs = getche();
-        scanf("%c%s", &gb, exp[i].rhs);
+        expr[i].lhs = getche();
+        scanf("%c%s", &gb, expr[i].rhs);
     }
 
     // printf("\nIntermediate Code :\n");
     printf("\n\n");
     for (i = 0; i < n; i++)
     {
-        printf("%c", exp[i].lhs);
-        printf("%s\n", exp[i].rhs);
+        printf("%c", expr[i].lhs);
+        printf("%s\n", expr[i].rhs);
     }
 
     for (i = 0; i < n - 1; i++)
     {
-        temp = exp[i].lhs;
+        temp = expr[i].lhs;
         for (j = 0; j < n; j++)
         {
-            p = strchr(exp[j].rhs, temp);
+            p = strchr(expr[j].rhs, temp);
             if (p)
             {
-                res[z].lhs = exp[i].lhs;
-                strcpy(res[z].rhs, exp[i].rhs);
+                res[z].lhs = expr[i].lhs;
+                strcpy(res[z].rhs, expr[i].rhs);
                 z++;
             }
         }
     }
 
-    res[z].lhs = exp[n - 1].lhs;
-    strcpy(res[z].rhs, exp[n - 1].rhs);
+    res[z].lhs = expr[n - 1].lhs;
+    strcpy(res[z].rhs, expr[n - 1].rhs);
     z++;
 
     printf("\nAfter dead code Removal :\n");
